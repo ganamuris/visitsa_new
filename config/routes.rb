0e6649004d2bfeb63871b6fff4943e66f68c8b42
Gana <gana_sa@hotmail.com>
2018-05-07 11:34:37 +0200

    added navbar and paths
    added navbar and paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root 'pages#landing_page'
   
+  get '/landing1_page' => 'pages#landing1_page'
+  get '/landing2_page' => 'pages#landing2_page'
+  get '/landing3_page' => 'pages#landing3_page'
+
 
 
 
