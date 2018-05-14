6ef364e356d0e41dbce7a58dd00fde2adc709ab7
Gana <gana_sa@hotmail.com>
2018-05-14 22:09:01 +0200

    Gana

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   get '/landing1_page' => 'pages#landing1_page'
   get '/landing2_page' => 'pages#landing2_page'
   get '/landing3_page' => 'pages#landing3_page'
-
-
-
+  get '/muzeji' => 'pages#muzeji'
+  get '/tabije' => 'pages#tabije'
+  get '/planine' => 'pages#planine'
+  get '/vjerski_objekti' => 'pages#vjerski_objekti'
+  get '/zooloski_vrt' => 'pages#zooloski_vrt'
+  get '/dani' => 'pages#dani' 
 
 
 
